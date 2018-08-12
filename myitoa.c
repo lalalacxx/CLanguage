36826922c597394bd0977fd187f9dd6a261e8674
root <root@localhost.localdomain>
2018-08-12 01:36:02 -0700

    模拟实现itoa

--- myitoa.c
+++ myitoa.c
@@ -2,13 +2,12 @@
 #include<assert.h>
 #include<stdlib.h>
 
-
-
 char *reverse(char *s)
 {
     assert(s);
-    char *head = s;
-    char *tail = s;
+    char *str = s;
+    char *head = str;
+    char *tail = str;
     while(*tail != '\0')
         tail++;//此时tail已经走到了'\0'的位置
     tail--;//因此循环结束以后经tail往后移动一个
@@ -18,13 +17,13 @@ char *reverse(char *s)
         *head++ = *tail;
         *tail-- = tmp;
     }
+    s = str;
     return s;
 }
-char *myitoa(int num)
+char *myitoa(int num,char *s)
 {
     int i = 0;
     int flag = 1;
-    char s[100];
     if(num < 0)
     {
         num = -num;//先将其转化为正数
@@ -42,17 +41,19 @@ char *myitoa(int num)
     s[i] = '\0';//加上字符串的'\0'
     //假如要转化的数字是-123，则走到这一步已经转化为"321-"
     //此时再将"321-"逆置就可得到"-123"
-    char *res = reverse(s);
-    return res;
+    reverse(s);
+    return s;
 }
 
 int main()
 {
     int a = 123;
     int b = -123;
-    char *s1 = myitoa(a);
+    char s1[255];
+    char s2[255];
+    myitoa(a,s1);
+    myitoa(b,s2);
     printf("123->%s\n",s1);
-    char *s2 = myitoa(b);
     printf("-123->%s\n",s2);
     return 0;
 }
