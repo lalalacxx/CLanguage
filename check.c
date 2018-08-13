1711a928a08d70b667bf7105b0cb448da0e0fb83
lalalacxx <2420059729@qq.com>
2018-08-12 18:07:32 -0700

    大小端判断

---
+++ check.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int check1()
+{
+    int i = 1;
+    return (*(char)&i);
+}
+//int check2()
+//{
+//    union
+//    {
+//        int i;
+//        char c;
+//    }un;
+//    un.i = 1;
+//    return un.c;
+//}
+int main()
+{
+    int res = check1();
+    //int res = check2();
+    if(res == 1)
+    {
+        printf("小端\n");
+    }
+    else
+    {
+        printf("大端\n");
+    }
+}
