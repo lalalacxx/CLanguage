92f215997439d481d3a4160f16c77f4c070f1c7a
lalalacxx <2420059729@qq.com>
2018-08-19 03:05:31 -0700

    模拟实现字符串系列函数

--- str_func.c
+++ str_func.c
@@ -92,7 +92,7 @@ int my_strcmp(const char *str1,const char *str2)
     assert(str2);
     unsigned char *s1 = (unsigned char *)str1;
     unsigned char *s2 = (unsigned char *)str2;
-    while(*str1 && *str2)
+    while(*s1 && *s2)
     {
         if(*s1 == *s2)
         {
